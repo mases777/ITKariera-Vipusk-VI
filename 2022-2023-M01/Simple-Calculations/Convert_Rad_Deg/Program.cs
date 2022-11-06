b3cd0c1489dbe815eb39c906a7015be697b14a40
mases777 <mases@abv.bg>
2022-11-06 10:19:18 +0200

    M01-03

---
+++ 2022-2023-M01/Simple-Calculations/Convert_Rad_Deg/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_Rad_Deg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var radian = double.Parse(Console.ReadLine());
+
+            var celsium = Math.Round(radian * 180 / Math.PI);
+
+            Console.WriteLine(celsium);
+
+        }
+    }
+}
