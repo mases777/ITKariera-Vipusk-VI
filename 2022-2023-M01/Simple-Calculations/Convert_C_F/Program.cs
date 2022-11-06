676a552495f759e46de37f85c5c92ec04b9e427c
mases777 <mases@abv.bg>
2022-11-06 10:11:21 +0200

    M01-02

---
+++ 2022-2023-M01/Simple-Calculations/Convert_C_F/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_C_F
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsium = double.Parse(Console.ReadLine());
+
+            var fahrenheit = celsium * 1.8 + 32;
+
+            Console.WriteLine(fahrenheit);
+
+        }
+    }
+}
