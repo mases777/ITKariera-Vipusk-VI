5091b886ec4fadca2651b5893b93384879b20b1c
mases777 <mases@abv.bg>
2022-11-06 09:48:01 +0200

    M01-Simple

---
+++ 2022-2023-M01/Simple-Calculations/Square-Area/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            var area = a * a;
+
+            Console.WriteLine(area);
+
+        }
+    }
+}
