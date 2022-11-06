89abeb78875b6bd8f18ec99c4cefaeba69ba1d50
mases777 <mases@abv.bg>
2022-11-06 11:11:11 +0200

    M01-Exams

---
+++ 2022-2023-M01/Simple-Calculations/TrainingLab-Exam1-Problem1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingLab_Exam1_Problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var h = double.Parse(Console.ReadLine()) * 100;
+            var w = double.Parse(Console.ReadLine()) * 100;
+
+            var row = (int)(h / 120);
+            var col = (int)((w - 100) / 70);            
+
+            Console.WriteLine(row * col - 3);
+        }
+    }
+}
