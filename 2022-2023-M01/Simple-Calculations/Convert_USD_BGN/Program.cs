ae893e9d1047e576d805d3a8e60134e15ee7bc66
mases777 <mases@abv.bg>
2022-11-06 10:27:59 +0200

    M01-04

---
+++ 2022-2023-M01/Simple-Calculations/Convert_USD_BGN/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_USD_BGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dolars = double.Parse(Console.ReadLine());
+
+            var leva = dolars * 1.79549;
+
+            Console.WriteLine($"{leva:f2} BGN");
+
+        }
+    }
+}
