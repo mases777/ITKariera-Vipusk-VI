88b0624054df4fdc9bbe238412464684ccb0b030
mases777 <mases@abv.bg>
2022-11-06 10:54:30 +0200

    M01-06

---
+++ 2022-2023-M01/Simple-Calculations/Triangle_Area/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+            var h = double.Parse(Console.ReadLine());
+
+            var area = a * h / 2;
+
+            Console.WriteLine(Math.Round(area, 2));
+
+            //Console.WriteLine($"{area:f2}");
+
+        }
+    }
+}
