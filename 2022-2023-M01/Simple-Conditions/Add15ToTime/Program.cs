1b4d647b774d122c8d0041f2299cf06c4e1b81c3
mases777 <mases@abv.bg>
2022-11-12 10:19:09 +0200

    M01-ThreeEqual

---
+++ 2022-2023-M01/Simple-Conditions/Add15ToTime/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add15ToTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var h = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+
+            var minutes = h * 60 + m;
+            minutes += 15;
+
+            var hour = minutes / 60;
+            var min = minutes % 60;
+
+            if (hour > 23)
+            {
+                hour = hour % 24;
+            }
+            Console.Write(hour + ":");
+            if (min < 10)
+            {
+                Console.WriteLine("0" + min);
+                //Console.WriteLine($"0{min}");
+            }
+            else
+            {
+                Console.WriteLine(min);
+            }
+
+
+
+
+
+
+
+            //minutes = minutes + 15;
+            //minutes += 15;
+
+            //minutes++;
+            //minutes = minutes + 1;
+            //minutes += 1;
+            //minutes -= -1;
+
+        }
+    }
+}
---
+++ 2022-2023-M01/Simple-Conditions/ThreeEqualNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeEqualNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+
+            if (a == b && a == c)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
