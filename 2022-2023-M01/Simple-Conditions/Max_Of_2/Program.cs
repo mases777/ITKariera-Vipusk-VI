fabbd1dd4ed3b66a2bf4574b6e06c682d7cb47a1
mases777 <mases@abv.bg>
2022-11-12 09:36:36 +0200

    M01-Max-of-2

---
+++ 2022-2023-M01/Simple-Conditions/Max_Of_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max_Of_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1);
+            }
+            else
+            {
+                Console.WriteLine(num2);
+            }
+        }
+    }
+}
