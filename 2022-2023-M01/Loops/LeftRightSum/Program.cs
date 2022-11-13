ac0b371e32f7fa8e32f78cbef9dfcc31303507de
mases777 <mases@abv.bg>
2022-11-13 10:26:45 +0200

    M01-LeftRightSum

---
+++ 2022-2023-M01/Loops/LeftRightSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeftRightSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                var num = int.Parse(Console.ReadLine());
+                leftSum += num;
+            }
+            int rightSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                var num = int.Parse(Console.ReadLine());
+                rightSum += num;
+            }
+            if (Math.Abs(leftSum - rightSum) == 0)
+            {
+                Console.WriteLine("Yes, sum = " + leftSum);
+            }
+            else
+            {
+                Console.WriteLine("No, diff = " + Math.Abs(leftSum - rightSum));
+            }
+
+        }
+    }
+}
