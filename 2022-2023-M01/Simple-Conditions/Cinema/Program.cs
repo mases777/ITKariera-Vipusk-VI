abed225898aff9241605611177e80591d130b969
mases777 <mases@abv.bg>
2022-11-13 09:55:35 +0200

    M01-Cinema

---
+++ 2022-2023-M01/Simple-Conditions/Cinema/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var projection = Console.ReadLine();
+            var r = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+
+            var total = 0.0;
+
+            if (projection == "Premiere")
+            {
+                total = r * c * 12;
+            }
+            else if (projection == "Normal")
+            {
+                total = r * c * 7.5;
+            }
+            else
+            {
+                total = r * c * 5;
+            }
+            Console.WriteLine($"{total:f2} leva");
+        }
+    }
+}
