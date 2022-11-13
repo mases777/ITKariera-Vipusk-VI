bad0d75dc231d0ffa7b1413eaa469419a97c7036
mases777 <mases@abv.bg>
2022-11-13 09:48:34 +0200

    M01-InvalidNumber

---
+++ 2022-2023-M01/Simple-Conditions/InvalidNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvalidNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            if (!((num >= 100 && num <= 200) || num == 0))
+            {
+                Console.WriteLine("invalid");
+            }
+
+        }
+    }
+}
