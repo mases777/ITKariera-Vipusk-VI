67ce2048190c1572eb8e592b7f9010e559dbf63c
mases777 <mases@abv.bg>
2022-11-19 09:31:36 +0200

    M01-NumbersPowerOf2

---
+++ 2022-2023-M01/Advanced-Loops/NumbersPowerOf2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersPowerOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            int a = 1;
+            Console.WriteLine(a);
+            for (int i = 1; i <= n; i++)
+            {
+                a *= 2;
+                Console.WriteLine(a);
+            }
+
+
+            //for (int i = 0; i <= n; i++)
+            //{
+            //    Console.WriteLine(Math.Pow(2, i));
+            //}
+
+        }
+    }
+}
