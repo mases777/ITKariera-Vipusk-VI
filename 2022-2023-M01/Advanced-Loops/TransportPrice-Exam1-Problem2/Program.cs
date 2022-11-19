a146ce16e058bcc964169397fd4d7a9cabfcf14b
mases777 <mases@abv.bg>
2022-11-19 11:04:04 +0200

    M01-VegetableMarket_Exam2_Problem1

---
+++ 2022-2023-M01/Advanced-Loops/TransportPrice-Exam1-Problem2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportPrice_Exam1_Problem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var period = Console.ReadLine();
+            double price = 0;
+            if (n < 20)
+            {
+                price += 0.7;
+                if (period == "day")
+                {
+                    price += n * 0.79;
+                }
+                else
+                {
+                    price += n * 0.9;
+                }
+            }
+            else if (n < 100)            
+                price += n * 0.09;            
+            else            
+                price += n * 0.06;            
+            Console.WriteLine(price);
+        }
+    }
+}
---
+++ 2022-2023-M01/Advanced-Loops/VegetableMarket-Exam2-Problem1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VegetableMarket_Exam2_Problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var kgPriceVegetable = double.Parse(Console.ReadLine());
+            var kgPriceFruit = double.Parse(Console.ReadLine());
+            var totalKgVegetable = int.Parse(Console.ReadLine());
+            var totalKgFruit = int.Parse(Console.ReadLine());
+
+            var priceVegetable = kgPriceVegetable * totalKgVegetable;
+            var priceFruit = kgPriceFruit * totalKgFruit;
+            var price = priceVegetable + priceFruit;
+            var priceEuro = price / 1.94;
+            Console.WriteLine(priceEuro);
+
+        }
+    }
+}
