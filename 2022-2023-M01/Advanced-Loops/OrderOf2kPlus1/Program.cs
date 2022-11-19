9ef037c36ebbe24cbdec037c5583ba44af740c4e
mases777 <mases@abv.bg>
2022-11-19 09:38:39 +0200

    M01-OrderOf2kPlus1

---
+++ 2022-2023-M01/Advanced-Loops/OrderOf2kPlus1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderOf2kPlus1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            int num = 1;
+            while (n >= num)
+            {
+                Console.WriteLine(num);
+                num = num * 2 + 1;
+            }
+
+        }
+    }
+}
