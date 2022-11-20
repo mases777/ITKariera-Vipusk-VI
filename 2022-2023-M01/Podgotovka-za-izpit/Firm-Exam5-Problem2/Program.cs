d17a2c9876af9ccdc9521675e6008629900ade6a
mases777 <mases@abv.bg>
2022-11-20 09:31:48 +0200

    M01-Firm_Exam5_Problem2

---
+++ 2022-2023-M01/Podgotovka-za-izpit/Firm-Exam5-Problem2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firm_Exam5_Problem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var neededHours = int.Parse(Console.ReadLine());
+            var days = int.Parse(Console.ReadLine());
+            var persons = int.Parse(Console.ReadLine());
+
+            var hoursWork = days * 0.9 * 8;
+            var extraTime = persons * 2 * days;
+            var totalHours = (int)Math.Floor(hoursWork + extraTime);
+            if (totalHours >= neededHours)
+            {
+                Console.WriteLine($"Yes!{totalHours - neededHours} hours left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough time!{neededHours - totalHours} hours needed.");
+            }
+
+        }
+    }
+}
