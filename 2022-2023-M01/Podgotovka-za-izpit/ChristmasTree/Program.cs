29b65de0b102601ce9fb2c8fd9526fc913fc370e
mases777 <mases@abv.bg>
2022-11-20 10:57:24 +0200

    M01-ChristmasTree

---
+++ 2022-2023-M01/Podgotovka-za-izpit/ChristmasTree/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(new string(' ', n - i));
+                Console.Write(new string('*', i));
+                Console.Write(" | ");
+                Console.WriteLine(new string('*', i));
+            }
+
+
+
+// n=11            
+//          * | *
+//         ** | **
+//        *** | ***
+//       **** | ****
+//      ***** | *****
+//     ****** | ******
+//    ******* | *******
+//   ******** | ********
+//  ********* | *********
+// ********** | **********
+//*********** | ***********
+        }
+    }
+}
