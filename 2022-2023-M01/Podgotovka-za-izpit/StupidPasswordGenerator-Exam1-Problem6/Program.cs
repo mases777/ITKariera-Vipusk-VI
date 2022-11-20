eaf8944f4dbf5c92372f8706d4663f034e4dca6d
mases777 <mases@abv.bg>
2022-11-20 10:44:46 +0200

    M01-StupidPasswordGenerator_Exam1_Problem6

---
+++ 2022-2023-M01/Podgotovka-za-izpit/StupidPasswordGenerator-Exam1-Problem6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupidPasswordGenerator_Exam1_Problem6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            for (int a = 1; a <= first; a++)
+                for (int b = 1; b <= first; b++)
+                    for (char c = 'a'; c < 'a' + second; c++)
+                        for (char d = 'a'; d < 'a' + second; d++)
+                            for (int e = 1; e <= first; e++)
+                            {
+                                if (e > a && e > b)
+                                {
+                                    Console.Write($"{a}{b}{c}{d}{e} ");
+                                }                                
+                            }
+            Console.WriteLine();
+        }
+    }
+}
