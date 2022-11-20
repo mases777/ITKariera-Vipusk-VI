4f5bc6e92835d3e72963ad3c651f5dbbe9f533fe
mases777 <mases@abv.bg>
2022-11-20 10:09:46 +0200

    M01-Hospital-Exam5-Problem4

---
+++ 2022-2023-M01/Podgotovka-za-izpit/Hospital-Exam5-Problem4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Exam5_Problem4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var period = int.Parse(Console.ReadLine());
+
+            int doctors = 7;
+            int count = 1;
+            int treated = 0;
+            int untreated = 0;
+            for (int i = 0; i < period; i++)
+            {
+                if (count % 3 == 0 && untreated > treated)
+                {
+                    doctors++;
+                }
+                var patient = int.Parse(Console.ReadLine());
+                if (patient > doctors)
+                {
+                    treated += doctors;
+                    untreated += patient - doctors;
+                }
+                else
+                {
+                    treated += patient;
+                }
+                count++;
+            }
+
+            Console.WriteLine($"Treated patients: {treated}.");
+            Console.WriteLine($"Untreated patients: {untreated}.");
+        }
+    }
+}
