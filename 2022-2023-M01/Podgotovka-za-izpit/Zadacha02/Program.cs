eebf3e76fc44423011cd37b3054bba53f7f81601
mases777 <mases@abv.bg>
2022-11-26 09:35:53 +0200

    M01-Zadacha02

---
+++ 2022-2023-M01/Podgotovka-za-izpit/Zadacha02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var budget = double.Parse(Console.ReadLine());
+            var firstPresent = double.Parse(Console.ReadLine());
+
+            var secondPresent = 0.2 * firstPresent;
+            var thirdPresent = 0.7 * secondPresent;
+            var fourthPresent = 0.5 * secondPresent;
+            var total = firstPresent + secondPresent + thirdPresent + fourthPresent;
+
+            if (budget >= total)
+            {
+                Console.WriteLine($"Yes! {budget - total:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"No! {total - budget:f2} leva needed.");
+            }
+
+        }
+    }
+}
