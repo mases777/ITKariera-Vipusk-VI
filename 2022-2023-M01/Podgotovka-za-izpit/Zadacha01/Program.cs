27648369c00c21160ae28944fee0fe2792005903
mases777 <mases@abv.bg>
2022-11-26 09:27:15 +0200

    M01-Zadacha01

---
+++ 2022-2023-M01/Podgotovka-za-izpit/Zadacha01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var priceLaminate = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var length = double.Parse(Console.ReadLine());
+            var pricePad = double.Parse(Console.ReadLine());
+
+            var areaRoom = width * length;
+            var laminatePrice = priceLaminate * areaRoom;
+            var padPrice = pricePad * areaRoom;
+            var priceMontaj = 0.4 * (laminatePrice + padPrice);
+            var totalPrice = laminatePrice + padPrice + priceMontaj;
+
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+    }
+}
