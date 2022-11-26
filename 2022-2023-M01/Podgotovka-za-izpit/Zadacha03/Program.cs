c2aeddc75d52622081eef99af205b395065c2d31
mases777 <mases@abv.bg>
2022-11-26 09:48:50 +0200

    M01-Zadacha03

---
+++ 2022-2023-M01/Podgotovka-za-izpit/Zadacha03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var width = double.Parse(Console.ReadLine());
+            var length = double.Parse(Console.ReadLine());
+            var chambers = int.Parse(Console.ReadLine());
+            var firm = Console.ReadLine();
+
+            var total = width * length;
+            if (chambers == 3)
+            {
+                if (firm == "Dogramichka4You") total *= 12;
+                else if (firm == "TihoToplo") total *= 15;
+                else total *= 14;
+            }
+            else if (chambers == 4)
+            {
+                if (firm == "Dogramichka4You") total *= 15;
+                else if (firm == "TihoToplo") total *= 14;
+                else total *= 20;
+            }
+            else
+            {
+                if (firm == "Dogramichka4You") total *= 20;
+                else if (firm == "TihoToplo") total *= 18;
+                else total *= 22;
+            }
+            Console.WriteLine($"Goshko has to spend {total:f2} leva.");
+        }
+    }
+}
