a32e53d00e95c26701d080d243d4b260900c4625
mases777 <mases@abv.bg>
2022-11-26 10:33:30 +0200

    M01-Zadacha05

---
+++ 2022-2023-M01/Podgotovka-za-izpit/Zadacha05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            for (int a = 0; a <= 9; a++)
+                for (int b = 1; b <= 9; b += 2)
+                    for (int c = 4; c <= 7; c += 3)
+                        for (int d = 0; d <= 9; d++)
+                            if (a + d == n)
+                                Console.Write($"{a}{b}{c}{d} ");
+            Console.WriteLine();
+
+        }
+    }
+}
