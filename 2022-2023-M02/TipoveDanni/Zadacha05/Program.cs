e7b5aa1d4355916356959a5f59351ce5d8d91239
mases777 <mases@abv.bg>
2023-01-29 10:24:00 +0200

    M02-5

---
+++ 2022-2023-M02/TipoveDanni/Zadacha05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Zadacha05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int water = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                water += input;
+                if (water > 255)
+                {
+                    water -= input;
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+            Console.WriteLine(water);
+        }
+    }
+}
