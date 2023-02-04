851cb525bc84be960af74607a49210206f0961b3
mases777 <mases@abv.bg>
2023-02-04 10:08:21 +0200

    Array01

---
+++ 2022-2023-M02/Array/Zadacha01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            int sum = 0;
+            double average = 0;
+
+            min = arr.Min();
+            max = arr.Max();
+            sum = arr.Sum();
+            average = arr.Average();
+
+            Console.WriteLine($"Min = {min}");
+            Console.WriteLine($"Max = {max}");
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Average = {average}");
+        }
+    }
+}
