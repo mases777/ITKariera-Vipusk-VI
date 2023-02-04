9773c5d60ed6ac8249e7421325c973b3adf2377b
mases777 <mases@abv.bg>
2023-02-04 10:27:20 +0200

    Array03

---
+++ 2022-2023-M02/Array/Zadacha02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] count = new int[65535];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                count[arr[i]]++;
+            }
+            int maxValue = count[0];
+            int maxIndex = 0, maxLeft = 0;
+            for (int i = 0; i < count.Length; i++)
+            {
+                if (count[i] > maxValue)
+                {
+                    maxValue = count[i];
+                    maxIndex = i;
+                }
+                if (maxIndex > maxLeft)
+                {
+                    maxLeft = maxIndex;
+                }
+            }
+            Console.WriteLine(maxLeft);
+        }
+    }
+}
---
+++ 2022-2023-M02/Array/Zadacha03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zadacha03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine($"{input[i]} -> {input[i] - 97}");
+            }
+        }
+    }
+}
