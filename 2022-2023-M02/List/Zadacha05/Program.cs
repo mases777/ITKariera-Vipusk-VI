5ce92f4bce82b63ae4b52e62d13248e3845bce7b
mases777 <mases@abv.bg>
2023-02-05 10:07:26 +0200

    List05

---
+++ 2022-2023-M02/List/Zadacha05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zadacha05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split().ToList();
+            int sum = 0;
+            foreach (var item in nums)
+            {
+                sum += int.Parse(ReverseString(item));
+            }
+            Console.WriteLine(sum);
+        }
+
+        private static string ReverseString(string item)
+        {
+            char[] chars = item.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+    }
+}
