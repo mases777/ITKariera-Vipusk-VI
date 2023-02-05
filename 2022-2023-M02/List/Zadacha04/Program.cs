a30f9882c9721e0d380a6b896efb9041c902fa99
mases777 <mases@abv.bg>
2023-02-05 09:56:48 +0200

    List04

---
+++ 2022-2023-M02/List/Zadacha04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zadacha04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> result = new List<int>();
+
+            int min = nums.Min();
+            int max = nums.Max();
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] == min || nums[i] == max)
+                {
+                    result.Add(nums[i]);
+                }
+            }
+
+            result.Sort();
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+    }
+}
