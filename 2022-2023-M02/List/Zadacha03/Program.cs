a8d3efa8018af432d7e6fbf15e76a5669c0469ff
mases777 <mases@abv.bg>
2023-02-05 09:48:38 +0200

    List03

---
+++ 2022-2023-M02/List/Zadacha03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zadacha03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    Console.Write($"{nums[i]} ");
+                }
+            }
+
+        }
+    }
+}
