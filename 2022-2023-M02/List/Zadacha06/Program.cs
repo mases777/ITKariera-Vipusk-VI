26c26a150a10a34f6b198739ea4a870255a57a16
mases777 <mases@abv.bg>
2023-02-05 10:19:55 +0200

    List06

---
+++ 2022-2023-M02/List/Zadacha06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zadacha06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> result = new List<int>();
+            foreach (var item in nums)
+            {
+                if (Math.Sqrt(item) == (int)Math.Sqrt(item))
+                {
+                    result.Add(item);
+                }
+            }
+            result.Sort((a, b) => b.CompareTo(a));
+            Console.WriteLine(String.Join(" ", result));
+
+        }
+    }
+}
