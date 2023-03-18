9e5d9800b6e6d665d4ca93955c6533ce7d5f0c1a
mases777 <mases@abv.bg>
2023-03-18 10:27:48 +0200

    String01

---
+++ 2022-2023-M02/String/Zadacha01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine()
+                .Split().Select(int.Parse).ToArray();
+
+            int target = line[0];
+            int number = line[1];
+
+            Console.WriteLine(Convert(number, target));
+            Console.WriteLine(Convert1(number, target));
+        }
+
+        private static string Convert1(int number, int target)
+        {
+            double sum = 0, pow = 0;
+            while (number > 0)
+            {
+                var current = number % 10;
+                sum += current * Math.Pow(target, pow);
+                pow++;
+                number /= 10;
+            }
+            return sum.ToString();
+        }
+
+        private static string Convert(int number, int target)
+        {
+            string result = string.Empty;
+            while (number > 0)
+            {
+                var remainder = number % target;
+                result = string.Concat(remainder.ToString(), result);
+                number /= target;
+            }
+            return result;
+        }
+    }
+}
