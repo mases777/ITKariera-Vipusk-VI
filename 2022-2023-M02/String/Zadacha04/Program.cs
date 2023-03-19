5b1877ca5488ea76f7748c3e5172853d01334660
mases777 <mases@abv.bg>
2023-03-19 10:03:06 +0200

    String04

---
+++ 2022-2023-M02/String/Zadacha04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Zadacha04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            var template = Console.ReadLine();
+
+            while (true)
+            {
+                int counter = 0;
+                while (text.Contains(template))
+                {
+                    text = text.Remove(text.IndexOf(template), template.Length);
+                    counter++;
+                }
+                if (counter == 0)
+                {
+                    Console.WriteLine("No shake."); break;
+                }
+                if (counter > 1)
+                {
+                    Console.WriteLine("Shaked it.");
+                }
+                template = template.Remove(template.Length / 2);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ 2022-2023-M02/String/elhi4ka/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace elhi4ka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter number: ");
+                int n = int.Parse(Console.ReadLine());
+                char ch = '#';
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write(new string(' ', n - i));
+                    if (i % 2 == 0) ch = '$';
+                    else ch = '#';
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write(ch + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wrong input");
+            }            
+        }
+    }
+}
