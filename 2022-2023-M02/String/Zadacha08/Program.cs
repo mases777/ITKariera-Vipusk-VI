ac335a687cc3ddc3daa4f54a6c6ddbcc9296f5ef
mases777 <mases@abv.bg>
2023-03-19 11:07:58 +0200

    String08

---
+++ 2022-2023-M02/String/Zadacha08/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Zadacha08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var cols = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                double avg = 0;
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{matrix[i, j], 10}");
+                    avg += matrix[i, j];
+                }
+                avg /= cols;
+                Console.WriteLine($"{avg, 10}");
+            }
+            
+        }
+    }
+}
