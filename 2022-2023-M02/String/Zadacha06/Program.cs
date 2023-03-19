b779167cb9b3dd53ded757429c6cb9b4ce409ac9
mases777 <mases@abv.bg>
2023-03-19 10:25:04 +0200

    String06

---
+++ 2022-2023-M02/String/Zadacha06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zadacha06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var banWord = Console.ReadLine().Split();
+            var text = Console.ReadLine();
+
+            foreach (var item in banWord)
+            {
+                if (text.Contains(item))
+                {
+                    text = text.Replace(item, new string('*', item.Length));
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
