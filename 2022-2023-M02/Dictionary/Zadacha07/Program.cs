30fe11c63956754f159f546b2411d53869ba895d
mases777 <mases@abv.bg>
2023-04-01 10:49:59 +0300

    Dict07

---
+++ 2022-2023-M02/Dictionary/Zadacha07/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var times = Console.ReadLine()
+                .Split()
+                .Select(x => DateTime.ParseExact(x, "HH:mm", null))
+                .ToList()
+                .OrderBy(x => x);
+
+            Console.WriteLine(String.Join(", ", times.Select(x => x.ToString("HH:mm")).ToArray()));
+
+        }
+    }
+}
