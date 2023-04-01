36be60f217e5c48c5f0868e0c6082ee959694a29
mases777 <mases@abv.bg>
2023-04-01 11:01:51 +0300

    Dict08

---
+++ 2022-2023-M02/Dictionary/Zadacha08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = Console.ReadLine()
+                .Split().Select(int.Parse).ToArray();
+
+            var even = num.Where(x => x % 2 == 0).ToArray();
+            var avg = even.Average();
+
+            var num1 = even.Where(x => x <= avg).Select(y => y = y - 1).ToArray();
+            var num2 = even.Where(x => x > avg).Select(y => y = y + 1).ToArray();
+
+            var num3 = num1.Concat(num2);
+
+            Console.WriteLine(String.Join(" ", num3));
+
+        }
+    }
+}
