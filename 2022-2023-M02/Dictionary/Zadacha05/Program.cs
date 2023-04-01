d9509985dca05eff6bd22ba6a8e605add9cdaa82
mases777 <mases@abv.bg>
2023-04-01 10:30:47 +0300

    Dict05

---
+++ 2022-2023-M02/Dictionary/Zadacha05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var separators = new char[]
+            {
+                '.', ',', ':', ';', '(', ')', '[', ']', '"', '\'', '\\', '/', '!', '?', ' '
+            };
+
+            var input = Console.ReadLine()
+                .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.ToLower())
+                .ToList();
+
+            var output = input.Where(x => x.Length < 5)
+                .Distinct()
+                .OrderBy(x => x)
+                .ToList();
+
+            Console.WriteLine(String.Join(", ", output));
+
+        }
+    }
+}
