9e7bbfac8a46ce30c0338182580d9c03a7c59125
mases777 <mases@abv.bg>
2023-04-01 10:14:07 +0300

    Dict03

---
+++ 2022-2023-M02/Dictionary/Zadacha03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zadacha03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> nums = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int next = int.Parse(Console.ReadLine());
+                nums.Add(next);
+            }
+            Console.WriteLine($"Sum = {nums.Sum()}");
+            Console.WriteLine($"Min = {nums.Min()}");
+            Console.WriteLine($"Max = {nums.Max()}");
+            Console.WriteLine($"Average = {nums.Average()}");
+
+        }
+    }
+}
