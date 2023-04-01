c3feffb0b47f41f21b1d601b47ae05b747dd15fb
mases777 <mases@abv.bg>
2023-04-01 11:09:10 +0300

    Dict09

---
+++ 2022-2023-M02/Dictionary/Zadacha09/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var commands = Console.ReadLine().Split().ToArray();
+                if (commands[0] == "stop")
+                {
+                    break;
+                }
+                var email = Console.ReadLine();
+                var names = (string.Join(" ", commands));
+                if (email.Contains("bg") && commands[0] != "stop")
+                {
+                    Console.WriteLine($"{names} -> {email}");
+                }
+            }
+        }
+    }
+}
