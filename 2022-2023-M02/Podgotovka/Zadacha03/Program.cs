ef67b4b0fc6f4b04a6f7638ad3a7e4a8eb11e694
mases777 <mases@abv.bg>
2023-04-02 11:38:15 +0300

    Podgotovka03

---
+++ 2022-2023-M02/Podgotovka/Zadacha03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Zadacha03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string maxWord = "";
+            int maxScore = 0;
+            string input;
+            while ((input = Console.ReadLine()) != "END OF GAME")
+            {
+                int sum = 0;
+                if (input[input.Length - 1] == 'a') sum += 10;
+                if (input[input.Length - 1] == 'v') sum += 13;
+                if (input.Length >= 7) sum += 33;
+                else sum += 22;
+
+                if (sum > maxScore)
+                {
+                    maxScore = sum;
+                    maxWord = input;
+                }
+            }
+            Console.WriteLine($"Winner is name: {maxWord}");
+            Console.WriteLine($"Points: {maxScore}");
+        }
+    }
+}
