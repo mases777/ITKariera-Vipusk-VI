78120f58bd1e42d7ecd8136f127965754fd83f28
mases777 <mases@abv.bg>
2023-04-02 11:14:47 +0300

    Podgotovka01

---
+++ 2022-2023-M02/Podgotovka/Zadacha01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                var type = Console.ReadLine();
+                switch (type)
+                {
+                    case "computer":
+                        sum += 5899; break;
+                    case "chair":
+                        sum += 1699; break;
+                    case "desk":
+                        sum += 1789; break;
+                }
+            }
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
