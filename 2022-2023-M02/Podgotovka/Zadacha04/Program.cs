1e178901ffbf40c386598bce171c94c7bffc1217
mases777 <mases@abv.bg>
2023-04-02 11:59:37 +0300

    Podgotovka04

---
+++ 2022-2023-M02/Podgotovka/Zadacha04/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zadacha04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<string, int> playerStats = new Dictionary<string, int>();
+            SortedDictionary<string, int> playerStats = new SortedDictionary<string, int>();
+            char[] separators = new char[] { ' ', '-' };
+            string input;
+            while ((input = Console.ReadLine()) != "End of match")
+            {
+                var info = input
+                    .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string player = info[0];
+                int passes = int.Parse(info[1]);
+
+                if (playerStats.ContainsKey(player))
+                {
+                    playerStats[player] += passes;
+                }
+                else
+                {
+                    playerStats[player] = passes;
+                }
+            }
+
+            //foreach (var item in playerStats.OrderBy(x => x.Key))
+            foreach (var item in playerStats)
+            {
+                Console.WriteLine($"{item.Key} has passed {item.Value} passes.");
+            }
+
+        }
+    }
+}
