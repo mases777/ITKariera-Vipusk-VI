d793ce51d7d2501df6ff99738697540cc04678ab
mases777 <mases@abv.bg>
2023-04-08 11:09:56 +0300

    Podg07

---
+++ 2022-2023-M02/Podgotovka/Zadacha07/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadacha07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> wordLetter = new List<string>();
+            List<string> wordA = new List<string>();
+            List<string> others = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                var word = Console.ReadLine();
+                if (word[0] == word[word.Length - 1])
+                {
+                    wordLetter.Add(word);
+                }
+                int letterA = 0;
+                foreach (var item in word)
+                {
+                    if (item == 'a')
+                    {
+                        letterA++;
+                    }
+                }
+                if (letterA >= 2)
+                {
+                    wordA.Add(word);
+                }
+                if (word[0] != word[word.Length - 1] 
+                    && letterA < 2)
+                {
+                    others.Add(word);
+                }
+            }
+            Console.WriteLine(String.Join(", ", wordLetter));
+            Console.WriteLine(String.Join(", ", wordA));
+            Console.WriteLine(String.Join(", ", others));
+
+        }
+    }
+}
