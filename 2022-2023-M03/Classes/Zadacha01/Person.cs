8a7608e5c82757a2efc538878eca26f960f3ad82
mases777 <mases@abv.bg>
2023-05-07 10:08:15 +0300

    M03-Zadacha01

---
+++ 2022-2023-M03/Classes/Zadacha01/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadacha01
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public void IntroduceYourself()
+        {
+            Console.WriteLine($"Hello, my name is {Name} and i am {Age} years old.");
+        }
+
+
+    }
+}
---
+++ 2022-2023-M03/Classes/Zadacha01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person firstPerson = new Person();
+            //firstPerson.Name = Console.ReadLine();
+            //firstPerson.Age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Name: {firstPerson.Name}; Age: {firstPerson.Age}");
+
+            Person secondPerson = new Person();
+            secondPerson.Name = Console.ReadLine();
+            secondPerson.Age = int.Parse(Console.ReadLine());
+            secondPerson.IntroduceYourself();
+
+        }
+    }
+}
