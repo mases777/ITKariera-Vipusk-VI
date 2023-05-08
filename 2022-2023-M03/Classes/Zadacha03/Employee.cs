670ccd985675ffe9a3accd13cb2405670bbc27e8
mases777 <mases@abv.bg>
2023-05-08 10:02:12 +0300

    M03-Zad03

---
+++ 2022-2023-M03/Classes/Zadacha03/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadacha03
+{
+    public class Employee
+    {
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		private double salary;
+
+		public double Salary
+		{
+			get { return salary; }
+			set { salary = value; }
+		}
+		private string position;
+
+		public string Position
+		{
+			get { return position; }
+			set { position = value; }
+		}
+		private string department;
+
+		public string Department
+		{
+			get { return department; }
+			set { department = value; }
+		}
+		private string email;
+
+		public string Email
+		{
+			get { return email; }
+			set { email = value; }
+		}
+		private int age;
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+
+
+	}
+}
---
+++ 2022-2023-M03/Classes/Zadacha03/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zadacha03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split().ToArray();
+                int age = -1;
+                string email = "n/a";
+                if (line.Count() == 6)
+                {
+                    email = line[4];
+                    age = int.Parse(line[5]);
+                }
+                else if (line.Count() > 4)
+                {
+                    if (!int.TryParse(line[4], out age))
+                    {
+                        age = -1;
+                        if (!string.IsNullOrEmpty(line[4]))
+                            email = line[4];
+                    }
+                }
+
+                employees.Add
+                    (
+                    new Employee()
+                        {
+                            Name = line[0],
+                            Salary = double.Parse(line[1]),
+                            Position = line[2],
+                            Department = line[3],
+                            Email = email,
+                            Age = age
+                        }
+                    );
+            }
+
+            Dictionary<string, List<double>> departmentPartitioning = new Dictionary<string, List<double>>();
+            foreach (var item in employees)
+            {
+                if (!departmentPartitioning.ContainsKey(item.Department))
+                {
+                    departmentPartitioning[item.Department] = new List<double>();
+                }
+                departmentPartitioning[item.Department].Add(item.Salary);
+            }
+
+            Dictionary<string, double> averageSalaryByDepartments = new Dictionary<string, double>();
+            foreach (var item in departmentPartitioning.Keys)
+            {
+                averageSalaryByDepartments[item] = departmentPartitioning[item].Average();
+            }
+
+            var maxAverageSalaryDepartment = averageSalaryByDepartments
+                .OrderByDescending(x => x.Value).First();
+            Console.WriteLine($"Highest Average Salary: {maxAverageSalaryDepartment.Key}");
+
+            employees = employees
+                .OrderByDescending(x => x.Salary).ToList();
+
+            foreach (var item in employees)
+            {
+                if (item.Department == maxAverageSalaryDepartment.Key)
+                {
+                    Console.WriteLine($"{item.Name} {item.Salary:f2} {item.Email} {item.Age}");
+                }
+            }
+               
+
+        }
+    }
+}
