477d6f9903fdecfad8774deddce39eda46f599a5
mases777 <mases@abv.bg>
2023-05-08 10:16:33 +0300

    M03-Zad04

---
+++ 2022-2023-M03/Classes/Zadacha04/DateModifier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Zadacha04
+{
+    public class DateModifier
+    {
+		private DateTime first;
+
+		public DateTime First
+		{
+			get { return first; }
+			set { first = value; }
+		}
+		private DateTime second;
+
+		public DateTime Second
+		{
+			get { return second; }
+			set { second = value; }
+		}
+		public void InIt(string d1, string d2)
+		{
+			this.First = DateTime.ParseExact(d1, "yyyy MM dd", CultureInfo.InvariantCulture);
+			this.Second = DateTime.ParseExact(d2, "yyyy MM dd", CultureInfo.InvariantCulture);
+		}
+		public int GetDifference()
+		{
+			return Math.Abs(this.First.Subtract(this.Second).Days);
+		}
+
+	}
+}
---
+++ 2022-2023-M03/Classes/Zadacha04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zadacha04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string date1 = Console.ReadLine();
+            string date2 = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier();
+            dateModifier.InIt(date1, date2);
+            Console.WriteLine(dateModifier.GetDifference());
+        }
+    }
+}
