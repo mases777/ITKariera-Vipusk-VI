09058f0055354d81761b8f28fa4e544ed03d81dc
mases777 <mases@abv.bg>
2023-05-20 11:08:42 +0300

    M03-FlowerShop

---
+++ 2022-2023-M03/Classes/FlowerShop/Flower.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularExam
+{
+    class Flower
+    {
+        private string type;
+        private string color;
+        private double price;
+        public Flower(string type, string color, double price)
+        {
+            this.Type = type;
+            this.Color = color;
+            this.Price = price;
+        }
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set {
+                if (value > 100)
+                {
+                    throw new ArgumentException("Invalid flower price!");
+                }
+                price = value; }
+        }
+        public override string ToString()
+        {
+            return $"Flower {this.Type} with color {this.Color} costs {this.Price:f2}";
+        }
+    }
+}
---
+++ 2022-2023-M03/Classes/FlowerShop/FlowerStore.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularExam
+{
+    class FlowerStore
+    {
+        private string name;
+        private List<Flower> flowers;
+        public FlowerStore(string name)
+        {
+            this.Name = name;
+            this.flowers = new List<Flower>();
+        }
+        public string Name
+        {
+            get { return name; }
+            set {
+                if (value.Length < 6)
+                {
+                    throw new ArgumentException("Invalid flower store name!");
+                }
+                name = value; }
+        }
+
+
+        public void AddFlower(Flower flower)
+        {
+            flowers.Add(flower);
+        }
+
+        public bool SellFlower(Flower flower)
+        {
+            Flower flowerToRemove = flowers.FirstOrDefault(x => x.Type == flower.Type);
+            return flowers.Remove(flowerToRemove);
+        }
+
+        public double CalculateTotalPrice()
+        {
+            return flowers.Sum(x => x.Price);
+        }
+
+        public Flower GetFlowerWithHighestPrice()
+        {
+            return flowers.OrderByDescending(x => x.Price).First();
+        }
+
+        public Flower GetFlowerWithLowestPrice()
+        {
+            return flowers.OrderBy(x => x.Price).First();
+        }
+
+        public void RenameFlowerStore(string newName)
+        {
+            this.Name = newName;
+        }
+
+        public void SellAllFlowers()
+        {
+            flowers.Clear();
+        }
+
+        public override string ToString()
+        {
+            if (flowers.Count == 0)
+            {
+                return $"Flower store {this.Name} has no available flowers.";
+            }
+            string result = $"Flower store {this.Name} has {this.flowers.Count} flower/s:\n";
+            result += string.Join("\n", flowers);
+
+            return result;
+        }
+    }
+}
