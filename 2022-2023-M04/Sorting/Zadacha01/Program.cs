4d50245df9c04d5cd205bb4da7647d2dd999aa7a
mases777 <mases@abv.bg>
2023-06-17 10:37:21 +0300

    Sorting

---
+++ 2022-2023-M04/Sorting/Zadacha01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        public static Stopwatch sw = new Stopwatch();
+        static void Main(string[] args)
+        {
+            //var numbers = new int[] { 9, -9, 8, -8, 7, -7, 6, -6, 5, -5, 4, -4, 3, -3, 2, -2, 1, -1, 0 };
+
+            //Console.WriteLine(string.Join(" ", numbers));
+
+            //Sort.Shuffle(numbers);
+            //Console.WriteLine(string.Join(" ", numbers));
+
+            //Sort.Selection(numbers);
+            //Console.WriteLine(string.Join(" ", numbers));
+
+            const int n = 20000;
+            var numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = i + 1;
+            }
+
+            Sort.Shuffle(numbers);
+
+            sw.Restart();
+            Sort.Selection(numbers);
+            sw.Stop();
+            Console.WriteLine($"Selection: {sw.ElapsedMilliseconds}");
+
+            Sort.Shuffle(numbers);
+
+            sw.Restart();
+            Sort.Bubble(numbers);
+            sw.Stop();
+            Console.WriteLine($"Bubble: {sw.ElapsedMilliseconds}");
+        }
+    }
+}
---
+++ 2022-2023-M04/Sorting/Zadacha01/Sort.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Zadacha01
+{
+    public static class Help
+    {
+        public static void Swap<T>(T[] elements, int first, int second)
+        {
+            T temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+
+        public static bool IsLess(IComparable first, IComparable second)
+        {
+            return first.CompareTo(second) < 0;
+        }
+
+        public static bool IsSorted<T>(T[] elements) where T : IComparable
+        {
+            for (int i = 1; i < elements.Length; i++)
+            {
+                if (elements[i - 1].CompareTo(elements[i]) > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static class Sort
+    {
+        public static Random random = new Random();
+
+        public static void Shuffle<T>(T[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                int r = i + random.Next(0, elements.Length - i);
+                Help.Swap(elements, i, r);
+            }
+        }
+
+        public static void Selection<T>(T[] elements) where T : IComparable
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < elements.Length; j++)
+                {
+                    if (Help.IsLess(elements[j], elements[min]))
+                    {
+                        min = j;
+                    }
+                }
+                Help.Swap(elements, min, i);
+            }
+        }
+
+        public static void Bubble<T>(T[] elements) where T : IComparable
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    if (Help.IsLess(elements[i], elements[j]))
+                    {
+                        Help.Swap(elements, i, j);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
