8837ffbb62117eee594ede5103bc0b0673a9ab57
mases777 <mases@abv.bg>
2023-06-18 09:59:23 +0300

    Search

---
+++ 2022-2023-M04/Sorting/Zadacha02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Zadacha02
+{
+    internal class Program
+    {
+        private static void MesureTime(Action metod)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            metod();
+            timer.Stop();
+            Console.WriteLine($"Time = {timer.ElapsedMilliseconds} ms");
+        }
+        static void Main(string[] args)
+        {
+            int index = 0;
+            int key = 54321987;
+            const int n = 100000000;
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = i + 1;
+            }
+
+            Console.WriteLine("Linear Seacrh ...");
+            MesureTime(() => index = Search.Linear(numbers, key));
+            Console.WriteLine(index == -1 ? false : true);
+
+            Console.WriteLine("Binary Seacrh ...");
+            MesureTime(() => index = Search.Binary(numbers, key));
+            Console.WriteLine(index == -1 ? false : true);
+
+        }
+    }
+}
---
+++ 2022-2023-M04/Sorting/Zadacha02/Search.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zadacha02
+{
+    public class Search
+    {
+        public static int Linear<T>(T[] elements, T key) where T : IComparable
+        {
+            for (int i = 0; i < elements.Count(); i++)
+            {
+                if (elements[i].CompareTo(key) == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static int Binary<T>(T[] elements, T key) where T : IComparable
+        {
+            int start = 0;
+            int end = elements.Count() - 1;
+            while (end >= start)
+            {
+                int mid = (start + end) / 2;
+                if (elements[mid].CompareTo(key) > 0)
+                {
+                    end = mid - 1;
+                }
+                else if (elements[mid].CompareTo(key) < 0)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+            return -1;
+
+        }
+
+    }
+}
