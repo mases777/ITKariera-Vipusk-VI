aa7ec7387e8763971fd899cb1453d0d07ef4568c
mases777 <mases@abv.bg>
2023-07-03 10:10:05 +0300

    M04-Izpit

---
+++ 2022-2023-M04/2023-07-03-Izpit/Bar/Bar.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UASD_Retake
+{
+    public class Bar
+    {
+        private string name;
+        private List<Cocktail> cocktails;
+
+        public Bar() { }
+
+        public Bar(string name)
+        {
+            Name = name;
+            Cocktails = new List<Cocktail>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public List<Cocktail> Cocktails
+        {
+            get { return cocktails; }
+            set { cocktails = value; }
+        }
+        public void AddCocktail(string name, double price)
+        {
+            Cocktail cocktail = new Cocktail(name, price);
+            cocktails.Add(cocktail);
+        }
+
+        public double AveragePriceInRange(double start, double end)
+        {
+            var average = cocktails.Where(x => x.Price >= start && x.Price <= end)
+                .Select(x => x.Price).Average();
+            return average;
+        }
+
+        public List<string> FilterCocktailsByPrice(double price)
+        {
+            return cocktails.Where(x => x.Price < price).Select(x => x.Name).ToList();
+        }
+
+        public List<Cocktail> SortAscendingByName()
+        {
+            cocktails = cocktails.OrderBy(x => x.Name).ToList();
+            return cocktails;
+        }
+
+        public List<Cocktail> SortDescendingByPrice()
+        {
+            cocktails = cocktails.OrderByDescending(x => x.Price).ToList();
+            return cocktails;
+        }
+
+        public bool CheckCocktailIsInBar(string name)
+        {
+            foreach (var item in cocktails)
+            {
+                if (item.Name == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string[] ProvideInformationAboutAllCocktails()
+        {
+            return cocktails.Select(x => x.ToString()).ToArray();
+        }
+    }
+}
---
+++ 2022-2023-M04/2023-07-03-Izpit/Bar/Cocktail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UASD_Retake
+{
+    public class Cocktail
+    {
+        private string name;
+        private double price;
+
+        public Cocktail() { }
+
+        public Cocktail(string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Cocktail {Name} costs {Price:f2} lv.";
+        }
+    }
+}
