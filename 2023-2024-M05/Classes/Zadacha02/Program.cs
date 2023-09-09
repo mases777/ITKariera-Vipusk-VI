26a64b4128575243c97adeb044d7ebf7bb56010e
mases777 <mases@abv.bg>
2023-09-09 10:09:57 +0300

    M05-Zadacha02

---
+++ 2023-2024-M05/Classes/Zadacha02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadacha02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rationals = new List<RationalNumber>();
+            for (int i = 0; i < 3; i++)
+            {
+                var n = int.Parse(Console.ReadLine());
+                var m = int.Parse(Console.ReadLine());
+                rationals.Add(
+                    new RationalNumber(n, m)
+                    ) ;
+            }
+            Console.WriteLine(string.Join("\n", rationals));
+
+        }
+    }
+}
---
+++ 2023-2024-M05/Classes/Zadacha02/RationalNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadacha02
+{
+    class RationalNumber
+    {
+		private int numerator;
+
+		public int Numerator
+		{
+			get { return numerator; }
+			set { numerator = value; }
+		}
+		private int denumerator;
+
+		public int Denumerator
+		{
+			get { return denumerator; }
+			set { denumerator = value; }
+		}
+
+        public RationalNumber(int numerator, int denumerator)
+        {
+            Numerator = numerator;
+            Denumerator = denumerator;
+        }
+
+        public override string ToString()
+        {
+			return $"{Numerator}/{Denumerator}";
+        }
+    }
+}
