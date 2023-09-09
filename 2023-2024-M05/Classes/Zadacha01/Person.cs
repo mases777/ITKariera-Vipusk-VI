f9b65712e69ceae0a6620ce1dbcc4e263a0ef3d9
mases777 <mases@abv.bg>
2023-09-09 09:52:24 +0300

    M05-Zadacha01

---
+++ 2023-2024-M05/Classes/Zadacha01/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadacha01
+{
+	class Person
+	{
+		private string name;
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		private int age;
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+		public void IntroduceYourself()
+		{
+			Console.WriteLine($"Здравейте! Аз съм {Name} и съм на {Age} години.");
+		}
+	}
+}
---
+++ 2023-2024-M05/Classes/Zadacha01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.Name = "Ivan";
+            person1.Age = 15;
+            person1.IntroduceYourself();
+
+            Person person2 = new Person();
+            person2.Name = Console.ReadLine();
+            person2.Age = int.Parse(Console.ReadLine());
+            person2.IntroduceYourself();
+        }
+    }
+}
