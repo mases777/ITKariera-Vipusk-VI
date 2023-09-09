63274ee0cef9f9241f45b543749936df4a8ccf8f
mases777 <mases@abv.bg>
2023-09-09 10:15:42 +0300

    M05-Zadacha02-2

--- 2023-2024-M05/Classes/Zadacha02/Program.cs
+++ 2023-2024-M05/Classes/Zadacha02/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Zadacha02
 {
@@ -8,15 +9,30 @@ namespace Zadacha02
         static void Main(string[] args)
         {
             var rationals = new List<RationalNumber>();
-            for (int i = 0; i < 3; i++)
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            for (int i = 0; i < input.Length; i += 2)
             {
-                var n = int.Parse(Console.ReadLine());
-                var m = int.Parse(Console.ReadLine());
                 rationals.Add(
-                    new RationalNumber(n, m)
-                    ) ;
+                    new RationalNumber(input[i], input[i + 1])
+                    );
             }
-            Console.WriteLine(string.Join("\n", rationals));
+            Console.WriteLine(string.Join("; ", rationals));
+
+
+
+            //var rationals = new List<RationalNumber>();
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    var n = int.Parse(Console.ReadLine());
+            //    var m = int.Parse(Console.ReadLine());
+            //    rationals.Add(
+            //        new RationalNumber(n, m)
+            //        ) ;
+            //}
+            //Console.WriteLine(string.Join("\n", rationals));
 
         }
     }
