7d44b5c3109d35458e42f4cfb2528938e4cb4039
mases777 <mases@abv.bg>
2023-09-10 10:16:58 +0300

    M05-Zadacha05

---
+++ 2023-2024-M05/Classes/Zadacha05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zadacha05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var primer1 = new Scale<int>(32, 41);
+            Console.WriteLine(primer1.GetHavier());
+
+            var primer2 = new Scale<string>("Ivan", "Petar");
+            Console.WriteLine(primer2.GetHavier());
+
+            var primer3 = new Scale<bool>(true, false);
+            Console.WriteLine(primer3.GetHavier());
+
+            var primer4 = new Scale<double>(2.34, 3.14);
+            Console.WriteLine(primer4.GetHavier());
+        }
+    }
+}
