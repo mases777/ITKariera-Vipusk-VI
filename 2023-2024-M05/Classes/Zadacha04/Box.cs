7bde717828e8d525ba9a66fbd3da8318e2cffa7e
mases777 <mases@abv.bg>
2023-09-10 09:54:41 +0300

    M05-Zadacha04

---
+++ 2023-2024-M05/Classes/Zadacha04/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zadacha04
+{
+    public class Box<T>
+    {
+        public int Count { get; private set; }
+
+        private List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+            this.Count = 0;
+        }
+        public void Add(T element)
+        {
+            list.Add(element);
+            this.Count++;
+        }
+        public T Remove()
+        {
+            var result = list.Last();
+            list.Remove(list.Last());
+            this.Count--;
+            return result;
+        }
+
+        public override string ToString()
+        {
+            string temp = null, type = null;
+            foreach (var item in list)
+            {
+                type = item.GetType().ToString();
+                temp += item.ToString();
+            };
+            return $"{type}: {temp}";
+
+        }
+    }
+}
--- 2023-2024-M05/Classes/Zadacha04/Program.cs
+++ 2023-2024-M05/Classes/Zadacha04/Program.cs
@@ -6,7 +6,17 @@ namespace Zadacha04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var box1 = new Box<int>();
+            box1.Add(123);
+            Console.WriteLine(box1.ToString());
+
+            var box2 = new Box<string>();
+            box2.Add("life in a box");
+            Console.WriteLine(box2.ToString());
+
+            var box3 = new Box<double>();
+            box3.Add(3.14);
+            Console.WriteLine(box3.ToString());
         }
     }
 }
