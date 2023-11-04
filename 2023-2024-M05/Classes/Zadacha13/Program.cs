a6ec4250a63259e256ceccc07c1e60469940dad4
mases777 <mases@abv.bg>
2023-11-04 11:21:35 +0200

    M05-Zadacha13

---
+++ 2023-2024-M05/Classes/Zadacha13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int count = numbers.Count();
+            Console.WriteLine(count);
+
+            int sum = numbers.Sum();
+            Console.WriteLine(sum);
+
+        }
+    }
+}
