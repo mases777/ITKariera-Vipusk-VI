a0ed0679bc000dd36a8ee22e30dd6b60720a5061
mases777 <mases@abv.bg>
2023-11-04 11:30:27 +0200

    M05-Zadacha14

---
+++ 2023-2024-M05/Classes/Zadacha14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace Zadacha14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine()
+                .Split();
+
+            Func<string, bool> checker = n => n[0] == n.ToUpper()[0];
+
+            words.Where(checker)
+                .ToList()
+                .ForEach(n => Console.WriteLine(n));
+
+        }
+    }
+}
