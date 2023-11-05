bb1f163f4e5b456203567c1cea2263ae261118f0
mases777 <mases@abv.bg>
2023-11-05 11:35:54 +0200

    M05-Zadacha15

---
+++ 2023-2024-M05/Classes/Zadacha15/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadacha15
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (value == null || value.Length == 0)
+                {
+                    throw new ArgumentNullException("FirstName cannot be empty.");
+                }
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (value == null || value.Length == 0)
+                {
+                    throw new ArgumentNullException("LastName cannot be empty.");
+                }
+                lastName = value;
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0 || value > 120)
+                {
+                    throw new ArgumentOutOfRangeException("Age must be between 0 and 120.");
+                }
+                age = value;
+            }
+        }
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+    }
+}
---
+++ 2023-2024-M05/Classes/Zadacha15/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Zadacha15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				Person per = new Person("", "Stephan", 12);
+			}
+			catch (ArgumentException ex)
+			{
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(new string('*', 33));
+
+            try
+			{
+				Person per = new Person("Stephi", "Stephanov", -12);
+			}
+			catch (ArgumentException ex)
+			{
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(new string('*', 33));
+
+            try
+            {
+                Person per = new Person("Stephi", "", 25);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
