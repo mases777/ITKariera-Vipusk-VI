bf9f8aa811fcbc6688d7579fb934b7aa4b3706ac
mases777 <mases@abv.bg>
2023-11-05 11:47:07 +0200

    M05-Zadacha16

---
+++ 2023-2024-M05/Classes/Zadacha16/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Zadacha16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("F:\\repos\\ITKariera-Vipusk-VI\\2023-2024-M05\\Classes\\Zadacha16\\text.txt");
+            using (reader)
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (counter % 2 == 1)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    counter++;
+                    line = reader.ReadLine();
+                }
+                reader.Close();
+            }
+
+        }
+    }
+}
