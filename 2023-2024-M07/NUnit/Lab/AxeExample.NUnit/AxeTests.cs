3af0867aa19863c4b1028e34af3f8e275a03ff94
Mehmed Ali Mehmed <mases@abv.bg>
2024-02-17 12:09:48 +0200

    Add files via upload

---
+++ 2023-2024-M07/NUnit/Lab/AxeExample.NUnit/AxeTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace AxeExample.NUnit
+{
+    public class AxeTests
+    {
+        [Test]
+        public void AxeLosesDurabilityAfterAttack()
+        {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            axe.Attack(dummy);
+
+            Assert.AreEqual(9, axe.DurabilityPoints, "Axe Durability Does Not Change After Attack.");
+        }
+
+        [Test]
+        public void BrokenAxeCantAttack()
+        {
+            Axe axe = new Axe(2, 2);
+            Dummy dummy = new Dummy(20, 20);
+
+            axe.Attack(dummy);
+            axe.Attack(dummy);
+
+            var ex = Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+            Assert.That(ex.Message, Is.EqualTo("Axe is broken."));
+        }
+    }
+}
---
+++ 2023-2024-M07/NUnit/Lab/AxeExample.NUnit/DummyTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace AxeExample.NUnit
+{
+    public class DummyTests
+    {
+        [Test]
+        public void DummyAxeLosesHealthAfterAttack()
+        {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            axe.Attack(dummy);
+
+            Assert.AreEqual(0, dummy.Health, "Dummy Health Does Not Change After Attack.");
+        }
+
+        [Test]
+        public void DeadDummyThrowsExeption()
+        {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(1, 1);
+
+            axe.Attack(dummy);
+
+            var ex = Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+            Assert.That(ex.Message, Is.EqualTo("Dummy is dead."));
+        }
+
+        [Test]
+        public void DeadDummyGivesExperiense()
+        {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(1, 50);
+
+            axe.Attack(dummy);
+
+            var ex = Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+            Assert.AreEqual(50, dummy.GiveExperience(), "Dead Dummy Does Not Gives Experience.");
+        }
+
+        [Test]
+        public void LiveDummyDoesNotGivesExperiense()
+        {
+            Axe axe = new Axe(1, 100);
+            Dummy dummy = new Dummy(10, 50);
+
+            axe.Attack(dummy);
+
+            var ex = Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+            Assert.That(ex.Message, Is.EqualTo("Target is not dead."));
+        }
+    }
+}
