8d0dc6bdc07f268918cab5e886ebada59319a507
Mehmed Ali Mehmed <mases@abv.bg>
2024-10-12 10:50:25 +0300

    Add files via upload

---
+++ 2024-2025-M10/Greedy/Zadacha05/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+internal class Program
+{
+    static List<bool[]> sign = new List<bool[]>();
+    static bool[] currentSolution;
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        var numbers = Console.ReadLine()
+            .Split().Select(int.Parse).ToArray();
+        currentSolution = new bool[n];
+        Permute(n, 0);
+        foreach (var item in sign)
+        {
+            var numGuess = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                numGuess[i] = (item[i] ? -1 : 1) * numbers[i];
+            }
+            if (numGuess.Sum() == 0)
+            {
+                Console.WriteLine($"{string.Join(" ", numGuess.Select(x => x > 0 ? $"+{x}" : $"{x}"))} = 0");
+            }
+        }
+    }
+
+    static void Permute(int n, int index)
+    {
+        if (n == index)
+        {
+            sign.Add(currentSolution.Clone() as bool[]);
+            return;
+        }
+
+        currentSolution[index] = true;
+        Permute(n, index + 1);
+        currentSolution[index] = false;
+        Permute(n, index + 1);
+
+    }
+}
---
+++ 2024-2025-M10/Greedy/Zadacha06/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+internal class Program
+{
+    static List<int> currentSolution = new List<int>();
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Solve(n, 0);
+    }
+
+    private static void Solve(int n, int index)
+    {
+        if (n <= 0)
+        {
+            for (int i = 0; i < currentSolution.Count-1; i++)
+            {
+                if (currentSolution[i] < currentSolution[i+1])
+                {
+                    return;
+                }
+            }
+            Console.WriteLine(string.Join(" + ", currentSolution));
+            return;
+        }
+        for (int i = n; i > 0; i--)
+        {
+            currentSolution.Add(i);
+            Solve(n - i, index + 1);
+            currentSolution.RemoveAt(index);
+        }
+    }
+}
